a4010e2b0eed8aa59011a12312e049b9c7d3dbf9
MatheusZamo <19matheushenrique03@gmail.com>
2025-04-01 18:10:35 -0300

    Base code

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,58 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+import { useState } from 'react'
+
+const games = [
+  {
+    id: '610aw15JvKL',
+    name: `Assassin's Creed Mirage`,
+    description: `Em Assassin's Creed Mirage, você é Basim, um astucioso ladino de rua em busca de respostas e de justiça.`,
+    imgUrl: 'https://m.media-amazon.com/images/I/610aw15JvKL._AC_SL1000_.jpg',
+  },
+  {
+    id: '61te8AW6zjL',
+    name: 'EA Sports FC 24',
+    description: 'O EA SPORTS FC 24 traz para você o Jogo de Todo Mundo.',
+    imgUrl: 'https://m.media-amazon.com/images/I/61te8AW6zjL._AC_SL1020_.jpg',
+  },
+  {
+    id: '81RfcW3Ml-L',
+    name: `Marvel's Spider-Man 2`,
+    description: `Peter Parker e Miles Morales retornam para uma nova e emocionante aventura na aclamada franquia de Marvel's Spider-Man.`,
+    imgUrl: 'https://m.media-amazon.com/images/I/81RfcW3Ml-L._AC_SL1500_.jpg',
+  }
+]
+
+const GamesList = ({ imgHeight }) =>
+  <ul>
+    {games.map(game =>
+      <li key={game.id}>
+        <Game game={game} imgHeight={imgHeight} />
+      </li>
+    )}
+  </ul>
+
+const Game = ({ game, imgHeight }) =>
+  <>
+    <Img game={game} imgHeight={imgHeight} />
+    <p><b>{game.name}</b>{': ' + game.description}</p>
+  </>
+
+const Img = ({ game, imgHeight }) =>
+  <img src={game.imgUrl} alt={game.name} style={{ height: imgHeight }} />
+
+const App = () => {
+  const [isLarge, setIsLarge] = useState(false)
+  const imgHeight = isLarge ? 200 : 100
+  const handleChange = e => setIsLarge(e.target.checked)
+  return (
+    <>
+      <label>
+        <input type="checkbox" checked={isLarge} onChange={handleChange} />
+        Ver imagens maiores
+      </label>
+      <hr />
+      <GamesList imgHeight={imgHeight} />
+    </>
+  )
+}
 
 export { App }
